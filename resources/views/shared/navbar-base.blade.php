0c9de6572e0a57a18435f0fbc59f5ba59046136c
Antonius <atomicbomber.email@gmail.com>
2017-11-28 08:59:40 +0700

    Added brand logo to navbar.

--- resources/views/shared/navbar-base.blade.php
+++ resources/views/shared/navbar-base.blade.php
@@ -1,6 +1,9 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
-    <a class="navbar-brand" href="@yield("navbar-brand-url")"> CV. HK Mitra Mandiri Konsultan </a>
+    <a class="navbar-brand" href="@yield("navbar-brand-url")">
+        <img height="30px" width="30px" src="{{ asset("images/brand.png") }}" class="d-inline-block align-top">
+        CV. HK Mitra Mandiri Konsultan
+    </a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
