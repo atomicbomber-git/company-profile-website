728b26d2e8c2481d36bd2aafaf9c7a05403d98a2
James Patrick Keegan <atomicbomber.email@gmail.com>
2017-12-05 09:12:37 +0700

    Fixed copyright text.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,7 +142,7 @@
     <!-- Footer -->
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+        <p class="m-0 text-center text-white">Copyright &copy; CV. HK Mitra Mandiri Konsultan 2017</p>
       </div>
       <!-- /.container -->
     </footer>
