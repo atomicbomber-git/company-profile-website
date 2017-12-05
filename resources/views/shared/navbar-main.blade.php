3da4659b22244cf4da9de3158a699380c4919de0
James Patrick Keegan <atomicbomber.email@gmail.com>
2017-12-05 09:11:16 +0700

    Removed unused links.

--- resources/views/shared/navbar-main.blade.php
+++ resources/views/shared/navbar-main.blade.php
@@ -7,16 +7,16 @@
 @section("navbar-menus")
   <ul class="navbar-nav ml-auto">
     <li class="nav-item">
-      <a class="nav-link" href="#"> Mengenai Perusahaan </a>
+      <a class="nav-link" href="#">
+        <i class="fa fa-users"></i>
+        Anggota Tim
+      </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#"> Pelayanan </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#"> Kontak </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{ route("gallery") }}"> Galeri </a>
+      <a class="nav-link" href="{{ route("gallery") }}">
+        <i class="fa fa-image"></i>
+        Galeri
+      </a>
     </li>
   </ul>
 @endsection
