68c2d2686db358a1b9732ae3e59d055e2f4f617d
James Patrick Keegan <atomicbomber.email@gmail.com>
2017-12-05 14:56:06 +0700

    Refactoring.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,23 +11,18 @@ class MainController extends Controller
 {
     public function welcome()
     {
-        $latest_photos = Photo::orderBy("created_at", "desc")->limit(3)->get();
-
         $promotional_texts = [];
         $promotional_texts[] = Label::fetchByTagname("promotional_1");
         $promotional_texts[] = Label::fetchByTagname("promotional_2");
         $promotional_texts[] = Label::fetchByTagname("promotional_3");
-        $welcome_text = Label::fetchByTagname("welcome");
-        $phone = Label::fetchByTagname("phone")->caption;
-        $email = Label::fetchByTagname("email")->caption;
 
         return view("welcome", [
-            "slides" => Slide::all(),
+            "slides" => Photo::orderBy("created_at", "desc")->limit(3)->get(),
             "photos" => $latest_photos,
             "promotional_texts" => $promotional_texts,
-            "welcome_text" => $welcome_text,
-            "phone" => $phone,
-            "email" => $email
+            "welcome_text" => Label::fetchByTagname("welcome"),
+            "phone" => Label::fetchByTagname("phone")->caption,
+            "email" => Label::fetchByTagname("email")->caption
         ]);
     }
 
